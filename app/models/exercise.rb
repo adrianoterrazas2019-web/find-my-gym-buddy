70e6282a1d48b7d39e1ad10bebd6402a463df759
Carl Furthmann <carl.furthi@gmail.com>
2026-06-02 14:33:04 +0200

    Update app/models/exercise.rb
    Co-authored-by: damgataum <ddsantos@gmx.de>

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,4 @@
 class Exercise < ApplicationRecord
+  validates :difficulty, inclusion: { in: %w(Beginner Intermediate Advanced), # rubocop:disable Style/PercentLiteralDelimiters
+                                  message: "%{value} is not a valid difficulty" } # rubocop:disable Style/FormatStringToken
 end
