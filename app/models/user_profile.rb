ce5517d09dbf320c0a3f4404cb8b46faf8c54ef3
adriano terrazas <adrianoterrazas2019@gmail.com>
2026-06-02 16:56:44 +0200

    fix birth validation

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -3,7 +3,7 @@ class UserProfile < ApplicationRecord
 
   validates :name, :birthdate, :gender, presence: true
   validates :name, length: { minimum: 2 }
-  validates :birthdate, comparison: { less_than: 18.years.ago,
+  validates :birthdate, comparison: { less_than_or_equal_to: 18.years.ago,
                                       message: "You need to be 18 or older" }
   validates :gender, inclusion: { in: %w(male female non-binary other), # rubocop:disable Style/PercentLiteralDelimiters
                                   message: "%{value} is not a valid gender" } # rubocop:disable Style/FormatStringToken
