351859abb32f1c9b7e0ae4850d7a2e713d9468da
adriano terrazas <adrianoterrazas2019@gmail.com>
2026-06-02 15:43:13 +0200

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-=======
   before_action :authenticate_user!
 
   private
@@ -9,5 +7,4 @@ class ApplicationController < ActionController::Base
     RubyLLM.models.chat_models.all
            .sort_by { |model| [model.provider.to_s, model.name.to_s] }
   end
->>>>>>> 3372a717391dc8a00e3786a229575baf3f635819
 end
