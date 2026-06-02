927df7f2740a746207962a610c55b607bb81045c
Daniel dos Santos <ddsantos@gmx.de>
2026-06-02 12:46:01 +0200

    implement user_profiles

---
+++ app/models/user_profile.rb
@@ -0,0 +1,10 @@
+class UserProfile < ApplicationRecord
+  belongs_to :user
+
+  validates :name, :birthdate, :gender, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :birthdate, comparison: { greater_than: 18.years.ago,
+                                      message: "You need to be 18 or older" }
+  validates :gender, inclusion: { in: %w(male female non-binary other), # rubocop:disable Style/PercentLiteralDelimiters
+                                  message: "%{value} is not a valid gender" } # rubocop:disable Style/FormatStringToken
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_02_081033) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_02_101009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -156,6 +156,19 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_02_081033) do
     t.index ["key"], name: "index_solid_queue_semaphores_on_key", unique: true
   end
 
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "address"
+    t.date "birthdate"
+    t.datetime "created_at", null: false
+    t.string "experience"
+    t.string "gender"
+    t.string "goal"
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -174,4 +187,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_02_081033) do
   add_foreign_key "solid_queue_ready_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_recurring_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_scheduled_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
+  add_foreign_key "user_profiles", "users"
 end
