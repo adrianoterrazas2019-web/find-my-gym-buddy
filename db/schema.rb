cf92320230c1974f5c73253337c226273d13adcb
Daniel dos Santos <ddsantos@gmx.de>
2026-06-03 17:33:06 +0200

    add embedding vector column to exercises

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_03_142144) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_153016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vector"
@@ -77,6 +77,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_03_142144) do
     t.datetime "created_at", null: false
     t.text "description"
     t.string "difficulty"
+    t.vector "embedding", limit: 1536
     t.string "equipment"
     t.string "target_muscle"
     t.string "title"
