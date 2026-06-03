33a54a39b4b76f000a396e13a407903510b4516a
damgataum <ddsantos@gmx.de>
2026-06-03 17:06:35 +0200

    Update app/models/user_profile.rb

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -11,7 +11,10 @@ class UserProfile < ApplicationRecord
                                       message: "You need to be 18 or older" }
   validates :gender, inclusion: { in: GENDERS,
                                   message: "%{value} is not a valid gender" } # rubocop:disable Style/FormatStringToken
-
+validates :goal, inclusion: { in: GOALS,
+                                  message: "%{value} is not a valid goal" } # rubocop:disable Style/FormatStringToken
+validates :experience, inclusion: { in: EXPERIENCES,
+                                  message: "%{value} is not a valid experience" } # rubocop:disable Style/FormatStringToken                                  
   scope :filter_by, ->(params) {
     results = all
     results = results.where("address ILIKE ?", "%#{params[:location]}%") if params[:location].present?
