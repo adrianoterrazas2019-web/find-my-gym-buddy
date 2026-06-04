267e638349f0424f6d11ff1de86d8165e8fa9a69
adrianoterrazas2019-web <adrianoterrazas2019@gmail.com>
2026-06-04 08:06:49 -0400

    Apply suggestions from code review
    Co-authored-by: damgataum <ddsantos@gmx.de>

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -20,7 +20,7 @@ class UserProfilesController < ApplicationController
     @profile = current_user.user_profile
 
     if @profile.update(profile_params)
-      redirect_to user_profiles_path,
+      redirect_to user_profile_path,
                   notice: "Profile updated."
     else
       render :edit,
