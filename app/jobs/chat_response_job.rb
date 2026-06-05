64586a89be0cf3cf3d2c806e43686bdad39cdab9
Daniel dos Santos <ddsantos@gmx.de>
2026-06-05 09:38:47 +0200

    Copy chat changes from Tom's bugfixes

--- app/jobs/chat_response_job.rb
+++ app/jobs/chat_response_job.rb
@@ -1,11 +1,11 @@
 class ChatResponseJob < ApplicationJob
-  def perform(chat_id, content, user_id = nil)
+  def perform(chat_id, content, user = nil)
     chat = Chat.find(chat_id)
-    Current.user = User.find_by(id: user_id) if user_id
 
     chat.ask(content) do |chunk|
       if chunk.content && !chunk.content.empty?
         message = chat.messages.last
+        message.user = user
         message.broadcast_append_chunk(chunk.content)
       end
     end
--- app/models/message.rb
+++ app/models/message.rb
@@ -5,17 +5,9 @@ class Message < ApplicationRecord
 
   broadcasts_to ->(message) { "chat_#{message.chat_id}" }, inserts_by: :append
 
-  before_create :assign_user_from_current
-
-  private
-
-  def assign_user_from_current
-    self.user ||= Current.user if role == "user"
-  end
-
   def broadcast_append_chunk(content)
     broadcast_append_to "chat_#{chat_id}",
-      target: "message_#{id}_content",
-      content: ERB::Util.html_escape(content.to_s)
+                        target: "message_#{id}_content",
+                        content: ERB::Util.html_escape(content.to_s)
   end
 end
