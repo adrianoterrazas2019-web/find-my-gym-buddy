56637ed5b333cfc734906173eced6b1c6adc109b
adriano terrazas <adrianoterrazas2019@gmail.com>
2026-06-08 16:25:33 +0200

    implement Authorization

--- app/controllers/workout_plans_controller.rb
+++ app/controllers/workout_plans_controller.rb
@@ -9,9 +9,13 @@ class WorkoutPlansController < ApplicationController
   end
 
   def show
-    @workout_plan = WorkoutPlan.joins(:pairing)
-                               .where(pairings: { id: current_user.pairings })
-                               .find(params[:id])
+    @workout_plan = WorkoutPlan.find_by(id: params[:id])
+
+    unless @workout_plan && current_user.pairings.exists?(@workout_plan.pairing_id)
+      redirect_to root_path, alert: "Not authorized."
+      return
+    end
+
     @workout_plan_exercises = @workout_plan.workout_plan_exercises.includes(:exercise)
   end
 end
