6e36bb1944b49ce7d1896360486943cf80b2bf13
Carl Furthmann <carl.furthi@gmail.com>
2026-06-08 15:19:37 +0200

    styling workoutplans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_04_140049) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_05_135527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vector"
@@ -340,6 +340,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_04_140049) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -356,6 +357,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_04_140049) do
     t.bigint "exercise_id", null: false
     t.integer "n_repetitions"
     t.integer "n_sets"
+    t.integer "rest_in_s"
     t.datetime "updated_at", null: false
     t.bigint "workout_plan_id", null: false
     t.index ["exercise_id"], name: "index_workout_plan_exercises_on_exercise_id"
