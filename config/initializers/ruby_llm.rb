0093722142c300adb0c7016da4c9c84dc4b7568e
Daniel dos Santos <ddsantos@gmx.de>
2026-06-09 15:27:14 +0200

    use openai api

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,7 +1,7 @@
 RubyLLM.configure do |config|
-  # config.openai_api_key = ENV["OPENAI_API_KEY"] # If you use this, comment lines 3 and 4
-  config.openai_api_key = ENV["GITHUB_TOKEN"]
-  config.openai_api_base = "https://models.inference.ai.azure.com"
+  config.openai_api_key = ENV["OPENAI_API_KEY"] # If you use this, comment lines 3 and 4
+  # config.openai_api_key = ENV["GITHUB_TOKEN"]
+  # config.openai_api_base = "https://models.inference.ai.azure.com"
   config.default_model = "gpt-4o-mini"
   config.default_embedding_model = "text-embedding-3-small"
   config.openai_use_system_role = true
